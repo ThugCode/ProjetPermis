b39b02a1d66e42aa4c946ebf50f47dd5976a75fe
LoicGerland <gerlandloic@gmail.com>
2016-06-09 11:13:20 +0200

    Les classes métiers

---
+++ src/main/java/com/project/permis/repositories/ActionRepository.java
@@ -0,0 +1,13 @@
+package com.project.permis.repositories;
+
+/**
+ * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
+ * @author Léo Letourneur (leo.letourneur@etu.univ-lyon1.fr)
+ * @author Thomas Arnaud (thomas.arnaud@etu.univ-lyon1.fr)
+ * @author Karl Gorgoglione (karl.gorgoglione@etu.univ-lyon1.fr)
+ * @author Loïc Gerland (loic.gerland@etu.univ-lyon1.fr)
+ * @author Guillaume Ogier (guillaume.ogier@etu.univ-lyon1.fr)
+ */
+public class ActionRepository extends AbstractRepository {
+
+}
