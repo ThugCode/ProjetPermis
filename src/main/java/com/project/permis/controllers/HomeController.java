4c8260a75792d7e6c3a4d9a028044a44450c2784
Letourneur Léo <letourneur.leo@gmail.com>
2016-06-15 11:41:23 +0200

    @Ogier

--- src/main/java/com/project/permis/controllers/HomeController.java
+++ src/main/java/com/project/permis/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.project.permis.controllers;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -12,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.project.permis.entities.Game;
+import com.project.permis.entities.Student;
+
 /**
  * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
- * @author Léo Letourneur (leo.letourneur@etu.univ-lyon1.fr)
+ * @author Lï¿½o Letourneur (leo.letourneur@etu.univ-lyon1.fr)
  * @author Thomas Arnaud (thomas.arnaud@etu.univ-lyon1.fr)
  * @author Karl Gorgoglione (karl.gorgoglione@etu.univ-lyon1.fr)
- * @author Loïc Gerland (loic.gerland@etu.univ-lyon1.fr)
+ * @author Loï¿½c Gerland (loic.gerland@etu.univ-lyon1.fr)
  * @author Guillaume Ogier (guillaume.ogier@etu.univ-lyon1.fr)
  */
 @Controller
@@ -40,6 +44,27 @@ public class HomeController extends AbstractController
 		
 		ModelMap model = new ModelMap();
 		model.addAttribute("serverTime", formattedDate);
+		
+		/*DonnÃ©es static en attendant les dynamiques*/
+		HashSet<Game> games = new HashSet<Game>();
+		Game a = new Game();
+		a.setName("Formation 1");
+		Game b = new Game();
+		b.setName("Formation 2");
+		Game c = new Game();
+		c.setName("Formation 3");
+		games.add(a);
+		games.add(b);
+		games.add(c);
+		Student user = new Student();
+		user.setId(-1);
+		user.setFirstname("LÃ©o");
+		user.setLastname("Letourneur");
+		user.setMail("letourneur.leo@gmail.com");
+		user.setGames(games);
+		/* Fin donnÃ©es statics*/
+		
+		model.addAttribute("user", user);
 		
 		return this.render("static/home", model);
 	}
