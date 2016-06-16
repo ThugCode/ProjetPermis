e3d3a52c8acda09e825060a83cf62f613df1232e
BrunoBuiret <bruno.buiret@gmail.com>
2016-06-16 16:28:49 +0200

    Revue de code, route liste des utilisateurs

--- src/main/java/com/project/permis/controllers/SecurityController.java
+++ src/main/java/com/project/permis/controllers/SecurityController.java
@@ -1,23 +1,18 @@
 package com.project.permis.controllers;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
- * @author Léo Letourneur (leo.letourneur@etu.univ-lyon1.fr)
+ * @author LÃ©o Letourneur (leo.letourneur@etu.univ-lyon1.fr)
  * @author Thomas Arnaud (thomas.arnaud@etu.univ-lyon1.fr)
  * @author Karl Gorgoglione (karl.gorgoglione@etu.univ-lyon1.fr)
- * @author Loïc Gerland (loic.gerland@etu.univ-lyon1.fr)
+ * @author LoÃ¯c Gerland (loic.gerland@etu.univ-lyon1.fr)
  * @author Guillaume Ogier (guillaume.ogier@etu.univ-lyon1.fr)
  */
 @Controller
---
+++ src/main/java/com/project/permis/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.project.permis.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
+ * @author Léo Letourneur (leo.letourneur@etu.univ-lyon1.fr)
+ * @author Thomas Arnaud (thomas.arnaud@etu.univ-lyon1.fr)
+ * @author Karl Gorgoglione (karl.gorgoglione@etu.univ-lyon1.fr)
+ * @author Loïc Gerland (loic.gerland@etu.univ-lyon1.fr)
+ * @author Guillaume Ogier (guillaume.ogier@etu.univ-lyon1.fr)
+ */
+@Controller
+public class UserController extends AbstractController
+{
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public ModelAndView list()
+	{
+		return this.render("user/list");
+	}
+}
