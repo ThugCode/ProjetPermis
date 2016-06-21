040c7c2ae5539de6d03e4011c0d9d91bca077444
BrunoBuiret <bruno.buiret@gmail.com>
2016-06-21 15:00:55 +0200

    Mise en commentaires des informations de liens

--- data.sql
+++ data.sql
@@ -4,10 +4,10 @@ INSERT INTO `action` (`id`, `name`) VALUES
 (3, 'Freiner'),
 (4, 'S''arrêter avant la ligne'),
 (5, 'Fermer les portes'),
-(6, 'Eviter un obstacle'),
-(7, 'Eviter un avion'),
-(8, 'Eviter les pietons'),
-(9, 'Eviter les valises'),
+(6, 'Éviter un obstacle'),
+(7, 'Éviter un avion'),
+(8, 'Éviter les pietons'),
+(9, 'Éviter les valises'),
 (10, 'Répondre aux voyageurs'),
 (11, 'Regarder le rétroviseur'),
 (12, 'Suivre les lignes'),
@@ -20,15 +20,15 @@ INSERT INTO `action` (`id`, `name`) VALUES
 (19, 'Utilise les clignotants');
 
 INSERT INTO `goal` (`id`, `name`) VALUES
-(1, 'Conduire sans risque'), 8, 11, 12, 15, 17, 19
-(2, 'Conduire doucement'), 1, 3, 15
-(3, 'Savoir faire les contrôles appropriés'), 16, 17
-(4, 'Savoir contrôler son bus'), 2,3,4,5,13
-(5, 'S''approprier la piste'), 16,18,12
+(1, 'Conduire sans risque'), -- 8, 11, 12, 15, 17, 19
+(2, 'Conduire doucement'), -- 1, 3, 15
+(3, 'Savoir faire les contrôles appropriés'), -- 16, 17
+(4, 'Savoir contrôler son bus'), -- 2, 3, 4, 5, 13
+(5, 'S''approprier la piste'), -- 16, 18, 12
 (6, 'Savoir éviter tout obstacle'),
-(7, 'Prendre toute l''information disponible'), 11,16,17,18
-(8, 'Être agréable'), 8,15,10
-(9, 'Transporter les voyageurs'), 5
+(7, 'Prendre toute l''information disponible'), -- 11, 16, 17, 18
+(8, 'Être agréable'), -- 8, 15, 10
+(9, 'Transporter les voyageurs'), -- 5
 (10, 'Réagir en cas d''incident'); 
 
 INSERT INTO `action_goal` (`id_action`, `id_goal`) VALUES
