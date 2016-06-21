528cb3786b96114a5fcf09d87b7d223a79ff5d87
BrunoBuiret <bruno.buiret@gmail.com>
2016-06-21 20:10:08 +0200

    Caractères UTF-8

--- src/main/java/com/project/permis/repositories/AbstractRepository.java
+++ src/main/java/com/project/permis/repositories/AbstractRepository.java
@@ -7,10 +7,10 @@ import org.hibernate.cfg.Configuration;
 
 /**
  * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
- * @author Léo Letourneur (leo.letourneur@etu.univ-lyon1.fr)
+ * @author LÃ©o Letourneur (leo.letourneur@etu.univ-lyon1.fr)
  * @author Thomas Arnaud (thomas.arnaud@etu.univ-lyon1.fr)
  * @author Karl Gorgoglione (karl.gorgoglione@etu.univ-lyon1.fr)
- * @author Loïc Gerland (loic.gerland@etu.univ-lyon1.fr)
+ * @author LoÃ¯c Gerland (loic.gerland@etu.univ-lyon1.fr)
  * @author Guillaume Ogier (guillaume.ogier@etu.univ-lyon1.fr)
  */
 public abstract class AbstractRepository
